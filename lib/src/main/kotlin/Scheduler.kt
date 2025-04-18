9e64ee4117d161772b05da6f98ce720c69c1b37e
akimolka <akimooolga@yandex.ru>
2025-04-18 15:45:15 +0300

    use newCachedThreadPool

--- lib/src/main/kotlin/Scheduler.kt
+++ lib/src/main/kotlin/Scheduler.kt
@@ -1,19 +1,22 @@
+import java.util.concurrent.Executors
 import kotlin.time.Duration
-import kotlin.collections.mutableListOf
-import kotlin.time.TimeMark
+
 import kotlin.time.TimeSource
 
 class Scheduler {
     val timeSource = TimeSource.Monotonic
-    var repository = InMemoryRepository()
+    val repository = InMemoryRepository()
+    val executor = Executors.newCachedThreadPool()
+
     fun scheduleAfter(duration: Duration, func: () -> Unit) {
         repository.add(ScheduledTask(func, timeSource.markNow() + duration))
     }
+
     fun run() {
         while (true) {
             val tasksToRun = repository.pickDueExecution()
             for (task in tasksToRun) {
-                task.func()
+                executor.submit(task.func)
             }
         }
     }
